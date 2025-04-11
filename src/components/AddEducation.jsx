4f33527032b1febfa10ec8a2a715c3c679cf36ed
astrum <astrum@iMac-astrum-261.local>
2025-04-11 11:56:27 +0500

    home

--- src/components/AddEducation.jsx
+++ src/components/AddEducation.jsx
@@ -2,102 +2,102 @@ import React from 'react'
 
 function AddEducation() {
   return (
-   <>
-   <dialog id="add_education" className="modal">
-                  <div className="modal-box p-5 text-left flex flex-col items-start border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10 ">
-                    <h3 className="font-bold text-lg">Ta'lim qo'shish</h3>
+    <>
+      <dialog id="add_education" className="modal">
+        <div className="modal-box p-5 text-left flex flex-col items-start border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10 ">
+          <h3 className="font-bold text-lg">Ta'lim qo'shish</h3>
 
-                    <fieldset className="fieldset w-full mt-5">
-                      <legend className="fieldset-legend text-sm flex items-center gap-1">
-                        Yonalish nomi
-                        <span className="text-red-500">*</span>
-                      </legend>
+          <fieldset className="fieldset w-full mt-5">
+            <legend className="fieldset-legend text-sm flex items-center gap-1">
+              Yonalish nomi
+              <span className="text-red-500">*</span>
+            </legend>
 
-                      <div className="relative w-full mt-2">
-                        <input
-                          type="text"
-                          className="input border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10  w-full pr-10 py-2"
-                          placeholder="Yonalishingizni yozing"
-                          required
-                        />
-                        <i className="bi bi-book-fill absolute right-3 top-1/2 transform -translate-y-1/2  px-1"></i>
-                      </div>
-                    </fieldset>
+            <div className="relative w-full mt-2">
+              <input
+                type="text"
+                className="input border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10  w-full pr-10 py-2"
+                placeholder="Yonalishingizni yozing"
+                required
+              />
+              <i className="bi bi-book-fill absolute right-3 top-1/2 transform -translate-y-1/2  px-1"></i>
+            </div>
+          </fieldset>
 
-                    <fieldset className="fieldset w-full mt-5">
-                      <legend className="fieldset-legend text-sm flex items-center gap-1">
-                        Oʻquv muassasasi nomi
-                        <span className="text-red-500">*</span>
-                      </legend>
-                      <div className="relative w-full mt-2">
-                        <input
-                          type="text"
-                          className="input border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10  w-full pr-10 py-2"
-                          placeholder=" Oʻquv muassasasi nomini kiriting"
-                          required
-                        />
-                        <i className="bi bi-buildings absolute right-3 top-1/2 transform -translate-y-1/2  px-1"></i>
-                      </div>
-                    </fieldset>
+          <fieldset className="fieldset w-full mt-5">
+            <legend className="fieldset-legend text-sm flex items-center gap-1">
+              Oʻquv muassasasi nomi
+              <span className="text-red-500">*</span>
+            </legend>
+            <div className="relative w-full mt-2">
+              <input
+                type="text"
+                className="input border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10  w-full pr-10 py-2"
+                placeholder=" Oʻquv muassasasi nomini kiriting"
+                required
+              />
+              <i className="bi bi-buildings absolute right-3 top-1/2 transform -translate-y-1/2  px-1"></i>
+            </div>
+          </fieldset>
 
-                    <legend className="fieldset-legend text-sm flex items-center gap-1 mt-5">
-                      Taʼlim turi
-                      <span className="text-red-500">*</span>
-                    </legend>
+          <legend className="fieldset-legend text-sm flex items-center gap-1 mt-5">
+            Taʼlim turi
+            <span className="text-red-500">*</span>
+          </legend>
 
-                    <select className="select select-bordered w-full mt-2 border dark:border-gray-600 border-slate-300 bg-[#F5F7FA] ">
-                      <option className="text-gray-950">Kollej</option>
-                      <option className="text-gray-500">Mactab</option>
-                      <option className="text-gray-500">Universitet</option>
-                    </select>
+          <select className="select select-bordered w-full mt-2 border dark:border-gray-600 border-slate-300 bg-[#F5F7FA] ">
+            <option className="text-gray-950">Kollej</option>
+            <option className="text-gray-500">Mactab</option>
+            <option className="text-gray-500">Universitet</option>
+          </select>
 
-                    <fieldset className="fieldset w-full mt-5">
-                      <legend className="fieldset-legend text-sm flex items-center gap-1">
-                        Muassasa veb sahifasi
-                      </legend>
+          <fieldset className="fieldset w-full mt-5">
+            <legend className="fieldset-legend text-sm flex items-center gap-1">
+              Muassasa veb sahifasi
+            </legend>
 
-                      <div className="relative w-full mt-2">
-                        <input
-                          type="text"
-                          className="input border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10  w-full pr-10 py-2"
-                          placeholder="https://"
-                          required
-                        />
-                        <i className="bi bi-link absolute right-3 top-1/2 transform -translate-y-1/2  px-1"></i>
-                      </div>
-                    </fieldset>
+            <div className="relative w-full mt-2">
+              <input
+                type="text"
+                className="input border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10  w-full pr-10 py-2"
+                placeholder="https://"
+                required
+              />
+              <i className="bi bi-link absolute right-3 top-1/2 transform -translate-y-1/2  px-1"></i>
+            </div>
+          </fieldset>
 
-                    <fieldset className="fieldset w-full mt-5">
-                      <legend className="fieldset-legend text-sm flex items-center gap-1">
-                        Muassasa veb sahifasi
-                      </legend>
+          <fieldset className="fieldset w-full mt-5">
+            <legend className="fieldset-legend text-sm flex items-center gap-1">
+              Muassasa veb sahifasi
+            </legend>
 
-                      <div className="relative w-full mt-2">
-                        <div className="relative">
-                          <input
-                            type="date"
-                            id="my-experience-date"
-                            className="input border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10  w-full pr-3 text-gray-500"
-                            required
-                          />
-                          <i className="bi bi-calendar absolute top-3 right-3 px-1"></i>
-                        </div>
-                      </div>
-                    </fieldset>
+            <div className="relative w-full mt-2">
+              <div className="relative">
+                <input
+                  type="date"
+                  id="my-experience-date"
+                  className="input border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10  w-full pr-3 text-gray-500"
+                  required
+                />
+                <i className="bi bi-calendar absolute top-3 right-3 px-1"></i>
+              </div>
+            </div>
+          </fieldset>
 
-                    <div className="modal-action w-full flex justify-end">
-                      <form method="dialog">
-                        <button className="rounded-lg px-2 py-1 hover:bg-gray-200 dark:hover:bg-gray-800  border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#2ec05a]/10 dark:from-[#eed9ed]/10 to-[#eed9ed]/20">
-                          Yopish
-                        </button>
-                      </form>
-                      <button className=" rounded-lg px-2 py-1 hover:bg-gray-200 dark:hover:bg-gray-800  border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#2ec05a]/10 dark:from-[#eed9ed]/10 to-[#eed9ed]/20">
-                        Qoshish
-                      </button>
-                    </div>
-                  </div>
-                </dialog>
-   </>
+          <div className="modal-action w-full flex justify-end">
+            <form method="dialog">
+              <button className="rounded-lg px-2 py-1 hover:bg-gray-200 dark:hover:bg-gray-800  border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#2ec05a]/10 dark:from-[#eed9ed]/10 to-[#eed9ed]/20">
+                Yopish
+              </button>
+            </form>
+            <button className=" rounded-lg px-2 py-1 hover:bg-gray-200 dark:hover:bg-gray-800  border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#2ec05a]/10 dark:from-[#eed9ed]/10 to-[#eed9ed]/20">
+              Qoshish
+            </button>
+          </div>
+        </div>
+      </dialog>
+    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,6 +38,281 @@ function Home() {
         </div>
       </>
 
+      <div>
+        <div className="text-center ">
+          <h3 className="font-bold text-4xl mb-6 ">Meet Our best Instructors</h3>
+          <p className="mb-8 text-sm ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, itaque. <br /> Labore tempora voluptates debitis laborum. Labore tempora voluptates debitis laborum</p>
+        </div>
+
+        <div className="grid grid-cols-3 gap-4 mb-4">
+
+          <div className="p-2 flex justify-between items-center rounded-xl border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10">
+            <div className="flex gap-3 items-center">
+              <div className="avatar ">
+                <div className="w-16 rounded-full">
+                  <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                </div>
+              </div>
+
+              <div>
+                <h1 className="text-xl font-bold">Zerda jursinova</h1>
+                <p className="text-xs">Frontend developer</p>
+              </div>
+            </div>
+
+
+            <div>
+              <i class="bi bi-arrow-up-right p-2 border rounded-xs hover:bg-gray-200 dark:hover:bg-gray-800  border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10"></i>
+            </div>
+
+          </div>
+
+
+          <div className="p-2 flex justify-between items-center rounded-xl border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10">
+            <div className="flex gap-3 items-center">
+              <div className="avatar ">
+                <div className="w-16 rounded-full">
+                  <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                </div>
+              </div>
+
+              <div>
+                <h1 className="text-xl font-bold">Zerda jursinova</h1>
+                <p className="text-xs">Frontend developer</p>
+              </div>
+            </div>
+
+
+            <div>
+              <i class="bi bi-arrow-up-right p-2 border rounded-xs hover:bg-gray-200 dark:hover:bg-gray-800  border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10"></i>
+            </div>
+
+          </div>
+
+
+          <div className="p-2 flex justify-between items-center rounded-xl border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10">
+            <div className="flex gap-3 items-center">
+              <div className="avatar ">
+                <div className="w-16 rounded-full">
+                  <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                </div>
+              </div>
+
+              <div>
+                <h1 className="text-xl font-bold">Zerda jursinova</h1>
+                <p className="text-xs">Frontend developer</p>
+              </div>
+            </div>
+
+
+            <div>
+              <i class="bi bi-arrow-up-right p-2 border rounded-xs hover:bg-gray-200 dark:hover:bg-gray-800  border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10"></i>
+            </div>
+
+          </div>
+
+
+          <div className="p-2 flex justify-between items-center rounded-xl border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10">
+            <div className="flex gap-3 items-center">
+              <div className="avatar ">
+                <div className="w-16 rounded-full">
+                  <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                </div>
+              </div>
+
+              <div>
+                <h1 className="text-xl font-bold">Zerda jursinova</h1>
+                <p className="text-xs">Frontend developer</p>
+              </div>
+            </div>
+
+
+            <div>
+              <i class="bi bi-arrow-up-right p-2 border rounded-xs hover:bg-gray-200 dark:hover:bg-gray-800  border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10"></i>
+            </div>
+
+          </div>
+
+
+          <div className="p-2 flex justify-between items-center rounded-xl border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10">
+            <div className="flex gap-3 items-center">
+              <div className="avatar ">
+                <div className="w-16 rounded-full">
+                  <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                </div>
+              </div>
+
+              <div>
+                <h1 className="text-xl font-bold">Zerda jursinova</h1>
+                <p className="text-xs">Frontend developer</p>
+              </div>
+            </div>
+
+
+            <div>
+              <i class="bi bi-arrow-up-right p-2 border rounded-xs hover:bg-gray-200 dark:hover:bg-gray-800  border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10"></i>
+            </div>
+
+          </div>
+
+
+          <div className="p-2 flex justify-between items-center rounded-xl border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10">
+            <div className="flex gap-3 items-center">
+              <div className="avatar ">
+                <div className="w-16 rounded-full">
+                  <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                </div>
+              </div>
+
+              <div>
+                <h1 className="text-xl font-bold">Zerda jursinova</h1>
+                <p className="text-xs">Frontend developer</p>
+              </div>
+            </div>
+
+
+            <div>
+              <i class="bi bi-arrow-up-right p-2 border rounded-xs hover:bg-gray-200 dark:hover:bg-gray-800  border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10"></i>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+
+
+      <div>
+        <div className="text-center p-3">
+          <h3 className="font-bold text-4xl mb-4">Most Popular Category </h3>
+        </div>
+
+        <div className="grid grid-cols-4 gap-4 mb-4">
+
+          <div className="p-3 flex justify-between items-center rounded-xl border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10">
+            <div className="flex gap-3 items-center">
+              <i class="bi bi-vector-pen text-xl"></i>
+
+              <div>
+                <h1 className="text-md font-normal ">Desing</h1>
+              </div>
+            </div>
+
+            <div>
+              <i class="bi bi-arrow-up-right p-1 border rounded-xs hover:bg-gray-200 dark:hover:bg-gray-800  border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10"></i>
+            </div>
+          </div>
+
+          <div className="p-3 flex justify-between items-center rounded-xl border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10">
+            <div className="flex gap-3 items-center">
+              <i class="bi bi-database text-xl"></i>
+
+
+              <div>
+                <h1 className="text-md font-normal "> Data Science</h1>
+              </div>
+            </div>
+
+            <div>
+              <i class="bi bi-arrow-up-right p-1 border rounded-xs hover:bg-gray-200 dark:hover:bg-gray-800  border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10"></i>
+            </div>
+          </div>
+
+          <div className="p-3 flex justify-between items-center rounded-xl border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10">
+            <div className="flex gap-3 items-center">
+              <i class="bi bi-file-code text-xl"></i>
+
+              <div>
+                <h1 className="text-md font-normal ">Development</h1>
+              </div>
+            </div>
+
+            <div>
+              <i class="bi bi-arrow-up-right p-1 border rounded-xs hover:bg-gray-200 dark:hover:bg-gray-800  border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10"></i>
+            </div>
+          </div>
+
+          <div className="p-3 flex justify-between items-center rounded-xl border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10">
+            <div className="flex gap-3 items-center">
+              <i class="bi bi-pie-chart text-xl"></i>
+
+              <div>
+                <h1 className="text-md font-normal ">Business</h1>
+              </div>
+            </div>
+
+            <div>
+              <i class="bi bi-arrow-up-right p-1 border rounded-xs hover:bg-gray-200 dark:hover:bg-gray-800  border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10"></i>
+            </div>
+          </div>
+
+          <div className="p-3 flex justify-between items-center rounded-xl border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10">
+            <div className="flex gap-3 items-center">
+              <i class="bi bi-image text-xl"></i>
+
+
+              <div>
+                <h1 className="text-md font-normal ">Photography</h1>
+              </div>
+            </div>
+
+            <div>
+              <i class="bi bi-arrow-up-right p-1 border rounded-xs hover:bg-gray-200 dark:hover:bg-gray-800  border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10"></i>
+            </div>
+          </div>
+
+          <div className="p-3 flex justify-between items-center rounded-xl border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10">
+            <div className="flex gap-3 items-center">
+              <i class="bi bi-music-note-beamed text-xl"></i>
+
+
+              <div>
+                <h1 className="text-md font-normal ">Music</h1>
+              </div>
+            </div>
+
+            <div>
+              <i class="bi bi-arrow-up-right p-1 border rounded-xs hover:bg-gray-200 dark:hover:bg-gray-800  border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10"></i>
+            </div>
+          </div>
+
+          <div className="p-3 flex justify-between items-center rounded-xl border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10">
+            <div className="flex gap-3 items-center">
+              <i class="bi bi-file-text text-xl"></i>
+
+              <div>
+                <h1 className="text-md font-normal ">Profisinal dev.</h1>
+              </div>
+            </div>
+
+            <div>
+              <i class="bi bi-arrow-up-right p-1 border rounded-xs hover:bg-gray-200 dark:hover:bg-gray-800  border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10"></i>
+            </div>
+          </div>
+
+          <div className="p-3 flex justify-between items-center rounded-xl border border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10">
+            <div className="flex gap-3 items-center">
+            <i class="bi bi-graph-up  text-xl"></i>
+
+              <div>
+                <h1 className="text-md font-normal ">Marketing</h1>
+              </div>
+            </div>
+
+            <div>
+              <i class="bi bi-arrow-up-right p-1 border rounded-xs hover:bg-gray-200 dark:hover:bg-gray-800  border-gray-300 dark:border-gray-600 bg-base-200 bg-gradient-to-r from-[#eed9ed]/60 dark:from-[#eed9ed]/10 to-[#2ec05a]/10"></i>
+            </div>
+          </div>
+
+
+
+
+
+
+
+
+        </div>
+      </div>
       <Footer />
     </div>
   );
